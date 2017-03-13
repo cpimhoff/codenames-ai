aab81254a6c6ea0a0724395a4eacaf9bad90b6f3
Graham Earley <graham.p.earley@gmail.com>
2017-03-13 11:53:09 -0500

    Fix HumanAgents indents

--- CodenamesAI/src/HumanAgents.kt
+++ CodenamesAI/src/HumanAgents.kt
@@ -1,48 +1,48 @@
 class HumanGuessAgent : GuessAgent {
 
-	override fun getGuess(wordsInPlay: List<String>, hint: Hint) : String {
-		println(wordsInPlay)
+    override fun getGuess(wordsInPlay: List<String>, hint: Hint) : String {
+        println(wordsInPlay)
 
-		// prompt
-		println("Give guess for hint: ")
+        // prompt
+        println("Give guess for hint: ")
 
-		// get input or exit if input is null
+        // get input or exit if input is null
         val guess = readLine() ?: kotlin.system.exitProcess(0)
-        
+
         return guess
-	}
+    }
 }
 
 class HumanHintAgent : HintAgent {
 
-	override fun getHint(board: Board, onRedTeam: Boolean) : Hint {
-		// print information to screen
-		board.printHinterView()
+    override fun getHint(board: Board, onRedTeam: Boolean) : Hint {
+        // print information to screen
+        board.printHinterView()
 
         val teamColor = if (onRedTeam) "Red" else "Blue"
 
-		// prompt
+        // prompt
         println("Give Hint for $teamColor Team:")
 
-		// loop until a successfully parsed hint
-		while (true) {
+        // loop until a successfully parsed hint
+        while (true) {
             // Read input, or exit if input is null:
             val input = readLine() ?: kotlin.system.exitProcess(0)
 
             try {
-				val tokens = input.split(" ")
-				val word = tokens[0]
-				val number = tokens[1].toInt()
+                val tokens = input.split(" ")
+                val word = tokens[0]
+                val number = tokens[1].toInt()
 
-				val hint = Hint(word, number)
-				return hint
+                val hint = Hint(word, number)
+                return hint
 
-			} catch (e: Exception) {
-				println("FORMAT: word integer")
-				continue	// try again
-			}
+            } catch (e: Exception) {
+                println("FORMAT: word integer")
+                continue	// try again
+            }
 
-			
-		}
-	}
+
+        }
+    }
 }
