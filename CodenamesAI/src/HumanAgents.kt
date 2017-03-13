a3ef62b31b3672ab23b4d7bafbb9c93b7af99b79
Charlie Imhoff <cpimhoff@gmail.com>
2017-03-12 19:26:06 -0500

    human agent impl.

---
+++ CodenamesAI/src/HumanAgents.kt
@@ -0,0 +1,55 @@
+class HumanGuessAgent : GuessAgent {
+
+	override fun getGuess(wordsInPlay: List<String>, hint: Hint) : String {
+		println(wordsInPlay)
+
+		// prompt
+		println("Give guess for hint: ")
+
+		// get input
+		val guess = readLine()
+		if (guess == null) {
+			kotlin.system.exitProcess(0)	// end of feed, exit
+		}
+
+		return guess
+	}
+}
+
+class HumanHintAgent : HintAgent {
+
+	override fun getHint(board: Board, onRedTeam: Boolean) : Hint {
+		// print information to screen
+		board.printHinterView()
+
+		// prompt
+		if (onRedTeam) {
+			println("Give Hint for Red Team:")
+		} else {
+			println("Give Hint for Blue Team:")
+		}
+
+		// loop until a successfully parsed hint
+		while (true) {
+			val input = readLine()
+			if (input == null) {
+				kotlin.system.exitProcess(0)	// end of feed, exit
+			}
+
+			try {
+				val tokens = input.split(" ")
+				val word = tokens[0]
+				val number = tokens[1].toInt()
+
+				val hint = Hint(word, number)
+				return hint
+
+			} catch (e: Exception) {
+				println("FORMAT: word integer")
+				continue	// try again
+			}
+
+			
+		}
+	}
+}
